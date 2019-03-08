f2de384f4c2b7d96e4a20357130018b5d63a269c
just-me- <hess-marcel@bluewin.ch>
2019-03-08 18:54:16 +0100

    Remove example

--- React/src/components/Login.js
+++ React/src/components/Login.js
@@ -66,32 +66,6 @@ class Login extends React.Component<Props, *> {
     return (
       <div>
         <h1>Bank of Rapperswil</h1>
-
-        // usage example
-        <List>
-          <List.Item>
-            <List.Icon name='users' />
-            <List.Content>Semantic UI</List.Content>
-          </List.Item>
-          <List.Item>
-            <List.Icon name='marker' />
-            <List.Content>New York, NY</List.Content>
-          </List.Item>
-          <List.Item>
-            <List.Icon name='mail' />
-            <List.Content>
-              <a href='mailto:jack@semantic-ui.com'>jack@semantic-ui.com</a>
-            </List.Content>
-          </List.Item>
-          <List.Item>
-            <List.Icon name='linkify' />
-            <List.Content>
-              <a href='http://www.semantic-ui.com'>semantic-ui.com</a>
-            </List.Content>
-          </List.Item>
-        </List>
-        // end example 
-
         <form>
           <h2>Login</h2>
           <input
