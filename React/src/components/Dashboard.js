22747c995906b3c53235b7449c84107dd55417e8
udfu <dario_delpiero10@hotmail.com>
2019-03-28 13:45:48 +0100

    show uid and amount

--- React/src/components/Dashboard.js
+++ React/src/components/Dashboard.js
@@ -5,6 +5,7 @@ import { Table, Grid, Header,  Segment,Button, Form } from 'semantic-ui-react'
 import * as api from "../api";
 import {DateFormat} from "./DateFormat";
 import type { User } from "./api";
+import {Menu} from "semantic-ui-react/dist/commonjs/collections/Menu/Menu";
 
 
 class Dashboard extends Component {
@@ -15,18 +16,18 @@ class Dashboard extends Component {
             tableData: [],
             column: null,
             direction: null,
-            filterYear: null,
+            amount: null,
             filterMonth: null,
             filterMsg: "",
             countTrans: 50,
-            user: user
+            user: JSON.parse(user)
         }
     }
 
     render() {
-        const user = sessionStorage.getItem("user");
 
-        const { column, tableData, direction } = this.state
+        const { column, tableData, amount, user } = this.state
+        const userAndAmount = user.accountNr + " - [" + amount + "]";
 
         return (
             <Segment placeholder>
@@ -36,8 +37,8 @@ class Dashboard extends Component {
                             Neue Bewegungen
                         </Header>
                         <Form>
-                            <Form.Input label='Von' placeholder='Sender Zahlung' />
-                            <Form.Input  label='Zu' placeholder='Empfänger Zahlung' />
+                            <Form.Input label='Von' value={userAndAmount}/>
+                            <Form.Input  label='Zu' placeholder='Empfänger Zahlung'/>
                             <Form.Input  label='Betrag - CHF' placeholder='Betrag Zahlung' />
                             <Button content='Überweisen' primary />
                         </Form>
@@ -98,6 +99,16 @@ class Dashboard extends Component {
                 });
             })
             .catch(error => console.log("Ups, ein Fehler ist aufgetreten", error));
+
+        api
+            .getAccountDetails(this.props.token)
+            .then(({amount}) => {
+                console.log(amount);
+                this.setState({
+                    amount: amount
+                });
+            })
+            .catch(error => console.log("Ups, ein Fehler ist aufgetreten", error))
     }
 }
 
