1ad5bfa7a3cde96c614854ab59afeed8083ceabf
just-me- <hess-marcel@bluewin.ch>
2019-04-13 10:18:37 +0200

    Menu... glaubs eher weniger schön... aber funktioniert xD

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -1,9 +1,22 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AuthService} from './auth/services/auth.service';
 
 @Component({
   selector: 'wed-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  public showMenu = false;
+
+  constructor(private autSvc: AuthService) {
+  }
+
+  ngOnInit() {
+    this.showMenu = this.autSvc.hasCredentials; // allgemein
+    this.autSvc.authenticatedUserChange.subscribe(
+      (credentials) => {
+        this.showMenu = this.autSvc.hasCredentials; // beim login selbst
+      });
+  }
 }
