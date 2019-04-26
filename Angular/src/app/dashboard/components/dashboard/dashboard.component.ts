a612e1ece09a4752ce35d861d8a6463ab091bf1b
just-me- <hess-marcel@bluewin.ch>
2019-04-26 14:52:13 +0200

    Inspiration... xD

--- Angular/src/app/dashboard/components/dashboard/dashboard.component.ts
+++ Angular/src/app/dashboard/components/dashboard/dashboard.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+import { TransactionResourceService } from '../../resources';
+import { Transaction } from '../../models';
+
+import { AuthService } from '../../../auth/services';
+import { Account } from '../../../auth/models';
 
 @Component({
   selector: 'wed-dashboard',
@@ -7,9 +14,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  public sourceNr: string;
+  public targetNr: string;
+  public amount: number;
+  public targetAccount: Account;
+
+  public errorMessage: string;
+  public successMessage: string;
+
+  constructor(private resource: TransactionResourceService, private auth: AuthService) {
+    if (auth.authenticatedUser) {
+      this.sourceNr = auth.authenticatedUser.accountNr;
+    }
+  }
 
   ngOnInit() {
+  }
+
+  public createTransaction(f: NgForm): boolean {
+    if (f && f.valid) {
+      this.errorMessage = this.successMessage = null;
+      this.resource.createTransaction(this.targetNr, this.amount).subscribe(
+        (transaction: Transaction) => {
+          if (transaction) {
+            this.targetNr = null;
+            this.amount = null;
+            this.targetAccount = null;
+            this.successMessage = `Transaction to ${transaction.target} succeeded! New balance ${transaction.total} CHF`;
+            this.transactionAdded.emit(transaction);
+          } else {
+            this.errorMessage = 'Could not transfer the money. Please check your transaction.';
+          }
+        }
+      );
+    }
+    return false;
+  }
+
+  public setTargetAccount() {
+    if (this.targetNr) {
+      this.resource.getAccount(this.targetNr).subscribe(
+        (account: Account) => {
+          this.targetAccount = account;
+        }
+      );
+    } else {
+      this.targetAccount = null;
+    }
   }
 
 }
