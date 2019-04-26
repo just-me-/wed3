df83b90b4ceedfb8b89bf4eff17e718cb02a2f07
udfu <dario_delpiero10@hotmail.com>
2019-04-26 14:40:56 +0200

    added service to component.... mocked not with real data...

--- Angular/src/app/dashboard/components/dashboard/dashboard.component.ts
+++ Angular/src/app/dashboard/components/dashboard/dashboard.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {TransactionService} from "../../services/transaction.service";
+import {NavigationService} from "@app/core";
+import {ActivatedRoute, Params} from "@angular/router";
+import {NgForm} from "@angular/forms";
+import {LoginInfo} from "../../../auth/models/login-info";
+import {Transaction} from "../../models/transaction";
 
 @Component({
   selector: 'wed-dashboard',
@@ -6,10 +12,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+  private backUrl;
 
-  constructor() { }
-
+  constructor(private transSvc: TransactionService,private navigationSvc: NavigationService, route: ActivatedRoute) {
+    route.params.subscribe(
+      (p: Params) => this.backUrl = p.backUrl);
+  }
   ngOnInit() {
   }
 
+  public doTransfer(f: NgForm){
+    if (f && f.valid) {
+      try {
+        this.transSvc.transfer(new Transaction("user1","user2",100, 1000,"2019-04-26-14:39:02"));
+      } catch (e) {
+        console.log("Something went wrong" + e);
+      }
+    }
+  }
 }
