17f4fa74995d50610ff15334209b11cca8dcec4e
just-me- <hess-marcel@bluewin.ch>
2019-05-09 16:49:30 +0200

    Inspiration..

--- Angular/src/app/transactions/transactions.component.ts
+++ Angular/src/app/transactions/transactions.component.ts
@@ -7,9 +7,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TransactionsComponent implements OnInit {
 
+  private lastYears: Array<any>;
+  private selectedYear: number;
+  private selectedMonth: number;
+
+  yearSelectionFieldChanged(event) {
+    this.selectedYear = parseInt(event.target.value);
+    this.startNewTransactionRequest();
+  }
+
+  monthSelectionFieldChanged(newValue) {
+    this.selectedMonth = parseInt(newValue);
+    this.startNewTransactionRequest();
+  }
+
+  startNewTransactionRequest() {
+    console.log(
+      "Transaktionen vom " +
+        this.selectedMonth +
+        " im Jahr " +
+        this.selectedYear +
+        " starten"
+    );
+  }
+
+  getLastYears() {
+    const thisYear = new Date().getFullYear();
+    return [thisYear, thisYear - 1, thisYear - 2];
+  }
+
   constructor() { }
 
   ngOnInit() {
+    console.log("Transactions onInit");
+    this.lastYears = this.getLastYears();
+    this.selectedYear = this.lastYears[0];
+    this.selectedMonth = new Date().getMonth() + 1;
+    this.startNewTransactionRequest();
   }
 
 }
