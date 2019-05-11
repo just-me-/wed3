6ef555b2af5d8f5c013183eae9193eee40fc01e8
just-me- <hess-marcel@bluewin.ch>
2019-05-11 22:00:33 +0200

    Formatieren

--- Angular/src/app/transactions/transactions.component.ts
+++ Angular/src/app/transactions/transactions.component.ts
@@ -24,7 +24,7 @@ export class TransactionsComponent implements OnInit {
     this.startNewTransactionRequest();
   }
 
-  daysInMonth (month, year = 2019) {
+  daysInMonth(month, year = 2019) {
     month = parseInt(month);
     return new Date(year, month, 0).getDate();
   }
@@ -38,17 +38,18 @@ export class TransactionsComponent implements OnInit {
         " starten"
     );
 
-    const month = (this.selectedMonth > 0) ? this.selectedMonth : "01";
-    const year = (this.selectedYear > 0) ? this.selectedYear : "2019";
+    const month = this.selectedMonth > 0 ? this.selectedMonth : "01";
+    const year = this.selectedYear > 0 ? this.selectedYear : "2019";
 
-    let dateFrom = (this.selectedYear > 0 || this.selectedMonth > 0 ? year : "2017")+"-"+month+"-01";
-    let dateTo = year+"-"
-                +(this.selectedMonth > 0 ? month : "12")
-                +"-"
-                +this.daysInMonth(month, this.selectedYear);
+    let dateFrom =
+      (this.selectedYear > 0 || this.selectedMonth > 0 ? year : "2017") +
+      "-" + month + "-01";
+    let dateTo =
+      year + "-" + (this.selectedMonth > 0 ? month : "12") + "-" +
+      this.daysInMonth(month, this.selectedYear);
+      
     console.log(dateFrom, dateTo);
     this.traSer.getTransactions(dateFrom, dateTo, 50, 0);
-
   }
 
   constructor(public traSer: TransactionService) {}
